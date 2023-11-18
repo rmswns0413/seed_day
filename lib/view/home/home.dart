897ce95fb1b2810234bd2441290d8df0808579bb
DESKTOP-8RH04EA\rmswns <rmswns0413@gmail.com>
2023-11-18 14:59:59 +0900

    SAFE AREA 추가

--- lib/view/home/home.dart
+++ lib/view/home/home.dart
@@ -19,49 +19,35 @@ class Home extends GetView<HomeController> {
   Widget build(BuildContext context) {
     return Obx(() => Scaffold(
           backgroundColor: Colors.black,
-          body: Container(
-            padding: const EdgeInsets.all(20),
-            child: Stack(
-              alignment: Alignment.center,
-              children: [
-                const Align(
-                  alignment: Alignment.topCenter,
-                  child: Text(
-                    'Welcome !!!',
-                    style: TextStyle(color: Colors.grey, fontSize: 60),
+          body: SafeArea(
+            child: Container(
+              padding: const EdgeInsets.all(20),
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  const Align(
+                    alignment: Alignment.topCenter,
+                    child: Text(
+                      'Welcome !!!',
+                      style: TextStyle(color: Colors.grey, fontSize: 60),
+                    ),
                   ),
-                ),
-                SizedBox(
-                  height: Get.height * 0.5,
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Expanded(
-                        flex: 1,
-                        child: Container(
-                            padding: const EdgeInsets.all(15),
-                            decoration: BoxDecoration(
-                                color: Colors.amber,
-                                borderRadius: BorderRadius.circular(15)),
-                            child: ClipRRect(
-                                borderRadius: BorderRadius.circular(10),
-                                child: Image.asset('assets/seed.png'))),
-                      ),
-                      Expanded(
-                        flex: 1,
-                        child: Container(
-                            margin: const EdgeInsets.only(left: 20),
-                            alignment: Alignment.center,
-                            decoration: BoxDecoration(
-                                color: Colors.amber,
-                                borderRadius: BorderRadius.circular(15)),
-                            child: Text('D', style: controller.textStyle)),
-                      ),
-                      Container(
-                          margin: const EdgeInsets.only(left: 20),
-                          child: Text(controller.prefix.value,
-                              style: controller.textStyle)),
-                      for (int i = 0; i < controller.day.length; i++)
+                  SizedBox(
+                    height: Get.height * 0.5,
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Expanded(
+                          flex: 1,
+                          child: Container(
+                              padding: const EdgeInsets.all(15),
+                              decoration: BoxDecoration(
+                                  color: Colors.amber,
+                                  borderRadius: BorderRadius.circular(15)),
+                              child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(10),
+                                  child: Image.asset('assets/seed.png'))),
+                        ),
                         Expanded(
                           flex: 1,
                           child: Container(
@@ -70,13 +56,29 @@ class Home extends GetView<HomeController> {
                               decoration: BoxDecoration(
                                   color: Colors.amber,
                                   borderRadius: BorderRadius.circular(15)),
-                              child: Text('${controller.day[i]}',
-                                  style: controller.textStyle)),
+                              child: Text('D', style: controller.textStyle)),
                         ),
-                    ],
-                  ),
-                )
-              ],
+                        Container(
+                            margin: const EdgeInsets.only(left: 20),
+                            child: Text(controller.prefix.value,
+                                style: controller.textStyle)),
+                        for (int i = 0; i < controller.day.length; i++)
+                          Expanded(
+                            flex: 1,
+                            child: Container(
+                                margin: const EdgeInsets.only(left: 20),
+                                alignment: Alignment.center,
+                                decoration: BoxDecoration(
+                                    color: Colors.amber,
+                                    borderRadius: BorderRadius.circular(15)),
+                                child: Text('${controller.day[i]}',
+                                    style: controller.textStyle)),
+                          ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ));
